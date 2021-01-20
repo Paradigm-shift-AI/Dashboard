aa408d114c0f0f8fc8363f64d9811916f48369c8
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2021-01-20 13:20:02 +0530

    updated icons

--- src/Plugins.js
+++ src/Plugins.js
@@ -57,7 +57,7 @@ const useStyles = makeStyles((theme) => ({
     padding: '2px 4px',
     display: 'flex',
     alignItems: 'center',
-    width: 400,
+    width: "100%",
     marginTop: "30px" 
   },
   input: {
